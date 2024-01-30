1ded86723c1e762a345bc319fdf1472bbf2c282b
Mutex <bluecity88@gmail.com>
2024-01-29 16:29:52 -1000

    WIP: splitting parentHash string into tspans correctly

--- src/Components/Poster/ParentHash.jsx
+++ src/Components/Poster/ParentHash.jsx
@@ -1,32 +1,46 @@
 import React from 'react';
 // import BlockUtils from "./BlockUtils";
 import {Buffer} from 'buffer'
+// import { overflowWrap } from 'html2canvas/dist/types/css/property-descriptors/overflow-wrap';
 
+const splitStringAtLength = (inputString, length) => {
+  let result = []
+  for (let i = 0; i < inputString.length; i += length) {
+    result.push(inputString.substr(i, length))
+  }
+  return result
+}
+
+const createTSpans = (strings) => {
+  return strings.map((string) => {
+    return '<<tspan>${string}</tspan>>'
+  })
+}
 
 const ParentHash = ({blockObject}) => {
-  console.log('blockObject:', blockObject)
-  console.log('header:', blockObject.header)
   
   const valueToHex = (value) => value === undefined ? false : Buffer.from(value).toString('hex')
   const parentHash = blockObject.header ? valueToHex(blockObject.header.parentHash) : null;
-
-  console.log('parentHash:', parentHash)
-  
+  const splitParentHashArr = splitStringAtLength(parentHash, 10)
+  const tspans = createTSpans(splitParentHashArr)
+  console.log('tspans:', tspans)
 
   return (
-    <div>
-      <span
-        style={{
-        color: 'white',
-        overflowWrap: 'break-word',
-        width: '100',
-        height: '100',
-        opacity: '50%'
-        }}
-      >
-        {parentHash}
-      </span>
-    </div>
+    <g>
+      <text
+        x={50}
+        y={400}
+        width='50'
+        heigh='100'
+        fill='black'
+        fontFamily="system-ui"
+        fontSize='16'
+        style={{overflowWrap: 'break-word'}}
+        transform='rotate(-25)'>
+        <tspan>12345</tspan>
+        <tspan>12345</tspan>
+      </text>
+          </g>
   )
 }
 
